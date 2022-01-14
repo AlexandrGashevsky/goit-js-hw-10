42a2ab4a55bd709539c4c9184197ce645e424702
AlexandrGashevsky <71451324+AlexandrGashevsky@users.noreply.github.com>
2022-01-14 23:19:12 +0200

    second

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './css/styles.css';
 import debounce from "lodash/debounce";
-//import Notiflix from 'notiflix';
 import {fetchCountries} from "./js/fetchCountries.js";
 const DEBOUNCE_DELAY = 300;
 const inputName = document.querySelector('#search-box');
